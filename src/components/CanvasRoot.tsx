fcadd94a56d04aa3b63412ff2d441398fc7f7c8e
George Satellite <lunaerxs@gmail.com>
2023-03-04 18:36:30 +0200

    remove post processing

--- src/components/CanvasRoot.tsx
+++ src/components/CanvasRoot.tsx
@@ -1,5 +1,4 @@
 import { Canvas } from '@react-three/fiber'
-import { Bloom, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 import { GameplayComponents } from './GameplayComponents'
 import {
   cameraShiftX,
@@ -17,11 +16,6 @@ export const CanvasRoot = () => {
     >
       <ambientLight intensity={0.9}/>
       <GameplayComponents/>
-      <EffectComposer>
-        <Bloom intensity={0.1} luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-        <Vignette offset={0.1} darkness={1.1} />
-        <Noise opacity={0.1} />
-      </EffectComposer>
     </Canvas>
   )
 }
