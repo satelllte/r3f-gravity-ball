bb6b7e52aeb6315defdbebd5a82d1e6338a32106
George Satellite <lunaerxs@gmail.com>
2023-03-04 20:07:41 +0200

    level4

--- src/components/levels/index.ts
+++ src/components/levels/index.ts
@@ -2,9 +2,11 @@ import type { Level } from '../types/Level'
 import { level1 } from './level1'
 import { level2 } from './level2'
 import { level3 } from './level3'
+import { level4 } from './level4'
 
 export const levels: Level[] = [
   level1,
   level2,
   level3,
+  level4,
 ]
