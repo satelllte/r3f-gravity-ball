9cd0daa37dd684bdb5d6d00394a8e0a73b8e1b11
George Satellite <lunaerxs@gmail.com>
2023-03-04 17:52:18 +0200

    postprocessing

--- src/components/CanvasRoot.tsx
+++ src/components/CanvasRoot.tsx
@@ -1,4 +1,5 @@
 import { Canvas } from '@react-three/fiber'
+import { Bloom, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
 import { GameplayComponents } from './GameplayComponents'
 import {
   cameraShiftX,
@@ -14,8 +15,13 @@ export const CanvasRoot = () => {
         position: [cameraShiftX, cameraShiftY, cameraShiftZ],
       }}
     >
-      <ambientLight/>
+      <ambientLight intensity={0.9}/>
       <GameplayComponents/>
+      <EffectComposer>
+        <Bloom intensity={0.1} luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        <Vignette offset={0.1} darkness={1.1} />
+        <Noise opacity={0.1} />
+      </EffectComposer>
     </Canvas>
   )
 }
