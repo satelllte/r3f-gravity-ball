a5b0cce2f4e1b3052254bd32b7abebee71436aca
George Satellite <lunaerxs@gmail.com>
2023-03-05 21:51:28 +0200

    remove crazy sdk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
 import Head from 'next/head'
 import { Game } from '@/components'
-import { useLayoutEffect } from 'react'
 
 const HomePage = () => {
-  useLayoutEffect(() => {
-    // @ts-expect-error
-    window.CrazyGames.CrazySDK.getInstance().init()
-  }, [])
   return (
     <>
       <Head>
@@ -14,8 +9,6 @@ const HomePage = () => {
         <meta name="description" content="Dark Ball" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src='https://sdk.crazygames.com/crazygames-sdk-v1.js'/>
       </Head>
       <Game/>
     </>
