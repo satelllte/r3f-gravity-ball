9fae0cdfe5d6724205e1bbfbc4d23928afdac2b3
George Satellite <lunaerxs@gmail.com>
2023-03-05 21:56:23 +0200

    doc title, description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ const HomePage = () => {
   return (
     <>
       <Head>
-        <title>Dark Ball</title>
-        <meta name="description" content="Dark Ball" />
+        <title>Gravity Ball</title>
+        <meta name="description" content="Gravity Ball browser game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
