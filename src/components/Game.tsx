337028f797b51fce98a48670c01345c1da0454d9
George Satellite <lunaerxs@gmail.com>
2023-03-05 21:58:09 +0200

    remove unnecessary todo

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,10 +7,6 @@ import { HideMouse } from './HideMouse'
 
 const CanvasRootLazy = dynamic(() =>
   import('./CanvasRoot').then((mod) => mod.CanvasRoot),
-  // TODO: add level loading screen
-  // {
-  //   loading: () => <>Loading...</>,
-  // }
 )
 
 export const Game = () => {
