36053eaedbf8bf132aaf765afb2710a40539d4e9
George Satellite <lunaerxs@gmail.com>
2023-03-06 17:50:52 +0200

    remove drag

--- src/components/CanvasRoot.tsx
+++ src/components/CanvasRoot.tsx
@@ -1,6 +1,4 @@
-import { createContext, createRef, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { FullGestureState, useDrag } from '@use-gesture/react'
 import { GameplayComponents } from './GameplayComponents'
 import {
   cameraShiftX,
@@ -8,31 +6,15 @@ import {
   cameraShiftZ,
 } from './constants'
 
-type DragState = Omit<FullGestureState<"drag">, "event"> & {
-  event: PointerEvent | MouseEvent | TouchEvent | KeyboardEvent
-}
-
-export const CanvasDragContext = createContext<React.RefObject<DragState | null>>(createRef())
-
 export const CanvasRoot = () => {
-  const dragRef = useRef<DragState | null>(null)
-
-  const bind = useDrag((args) => {
-    dragRef.current = args
-  })
-
   return (
     <Canvas
       camera={{
         position: [cameraShiftX, cameraShiftY, cameraShiftZ],
       }}
-      className='touch-none'
-      {...bind()}
     >
-      <CanvasDragContext.Provider value={dragRef}>
-        <ambientLight intensity={0.9}/>
-        <GameplayComponents/>
-      </CanvasDragContext.Provider>
+      <ambientLight intensity={0.9}/>
+      <GameplayComponents/>
     </Canvas>
   )
 }
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,9 @@
-import { forwardRef, useContext, useEffect, useImperativeHandle, useRef } from 'react'
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react'
 import { Mesh, RepeatWrapping, Vector3 } from 'three'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useSphere, Triplet } from '@react-three/cannon'
 import { useTexture } from '@react-three/drei'
 import { useRecoilState } from 'recoil'
-import { CanvasDragContext } from './CanvasRoot'
 import { useGetKeyboardControls } from './KeyboardControls'
 import { gameState, GameState } from './state'
 import {
@@ -33,8 +32,6 @@ export const Player = forwardRef<Mesh>((_, forwardedRef) => {
 
   const { camera } = useThree()
   const positionRef = useRef<Triplet>([0, 1, 0])
-
-  const dragRef = useContext(CanvasDragContext)
 
   const getKeyboardControls = useGetKeyboardControls()
 
@@ -74,7 +71,6 @@ export const Player = forwardRef<Mesh>((_, forwardedRef) => {
     }
 
     if (positionRef.current[1] < -12.5) {
-      dragRef.current?.cancel()
       setGameState(GameState.lost)
     }
 
@@ -100,21 +96,6 @@ export const Player = forwardRef<Mesh>((_, forwardedRef) => {
     if (right) {
       api.applyImpulse([force, 0, 0], centerPoint)
     }
-
-    if (!dragRef.current || dragRef.current.canceled) {
-      return
-    }
-
-    const [dragX, dragY] = dragRef.current.movement
-    if (!dragX && !dragY) {
-      return
-    }
-
-    api.applyImpulse([
-      force * 0.005 * dragX,
-      0,
-      force * 0.005 * dragY,
-    ], centerPoint)
   })
 
   const texture = useTexture('/texture-ball.png', (texture) => {
