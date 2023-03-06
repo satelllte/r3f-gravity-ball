adabf96d6ad57f2601d0128a16ac3a6e66dfd476
George Satellite <lunaerxs@gmail.com>
2023-03-06 22:51:14 +0200

    crazygames sdk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from 'next/head'
+import { useLayoutEffect } from 'react'
 import { Game } from '@/components'
 
 const HomePage = () => {
+
+  useLayoutEffect(() => {
+    // @ts-expect-error
+    const crazysdk = window.CrazyGames.CrazySDK.getInstance()
+    crazysdk.init()
+  }, [])
+
   return (
     <>
       <Head>
@@ -9,6 +17,8 @@ const HomePage = () => {
         <meta name="description" content="Gravity Ball browser game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="https://sdk.crazygames.com/crazygames-sdk-v1.js"/>
       </Head>
       <Game/>
     </>
